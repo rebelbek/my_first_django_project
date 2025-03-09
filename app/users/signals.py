58833834f54fc77574b6cd720bb18f898580f47b
rebelbek <rebelbek@mail.ru>
2025-03-09 12:25:29 +0300

    Убрал возможность обновлять данные для обычных пользователей

--- app/users/signals.py
+++ app/users/signals.py
@@ -8,9 +8,10 @@ from django.urls import reverse
 @receiver(post_save, sender=User)
 def user_update(sender, instance, created, *args, **kwargs):
     if created:
-        send_mail(subject='Вы зарегистрировались на сайте rebelbek.ru',
-            message='Если это были вы, нажмите на ссылку чтобы подтвердить вашу электронную почту:'
-            'http://rebelbek.ru%s' % reverse('verify', kwargs={'uuid': str(instance.verification_uuid)}),
+        send_mail(subject='rebelbek.ru',
+            message='Вы зарегистрировались на сайте rebelbek.ru. '
+                    'Eсли это были вы, нажмите на ссылку чтобы подтвердить вашу электронную почту: '
+                    'http://rebelbek.ru%s' % reverse('verify', kwargs={'uuid': str(instance.verification_uuid)}),
             from_email='rebelbek.stocks@mail.ru',
             recipient_list=[instance.email],
             fail_silently=False,
